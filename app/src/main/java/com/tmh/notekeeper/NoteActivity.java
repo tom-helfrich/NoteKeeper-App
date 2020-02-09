b8a264731e539e4812e37290019b5e11f325aabf
tom-helfrich <thomasmhelfrich@gmail.com>
2020-02-09 17:51:34 -0600

    First setup for master branch

--- app/src/main/java/com/tmh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/tmh/notekeeper/NoteActivity.java
@@ -25,7 +25,7 @@ public class NoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Placeholder text", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
